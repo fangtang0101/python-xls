fcf1a209f43e24f8a2420c1bbef6d4d41f173c26
fangchungao <fangchungao@rltx.com>
2018-03-01 18:46:41 +0800

    1.进一步

--- test.py
+++ test.py
@@ -1,19 +1,41 @@
 from xlutils.copy import copy
 import xlrd
+import sys
 
 
 
-book = xlrd.open_workbook('模版.xls',formatting_info=True)
-sheets=book.sheets()
+# book = xlrd.open_workbook('模版.xls',formatting_info=True)
+# sheets=book.sheets()
 
-sheet_A37 = book.sheet_by_name('37#')
+# sheet_A37 = book.sheet_by_name('37#')
 
-rows = sheet_A37.nrows
-cols = sheet_A37.ncols
+# rows = sheet_A37.nrows
+# cols = sheet_A37.ncols
 
-print('row',rows)
-print('col',rows)
+# print('row',rows)
+# print('col',rows)
 
-for row in range(rows-100):
-	cell = sheet_A37.cell_value(row,3)
-	print('cell value 哈哈:',cell)
+# # row 列
+# # col 行
+
+# list_cell = []
+
+# for row in range(rows):
+# 	cell = sheet_A37.cell_value(row,3)
+# 	if cell.strip() != '':
+# 		cell_target  = {'row': row, 'col': 3, 'name': cell }
+# 		list_cell.append(cell_target)
+
+	    
+# print(list_cell)
+
+
+# anther sheet find value
+
+book_source = xlrd.open_workbook('YP03销售量排名.xls',formatting_info=True,encoding_override=sys.getfilesystemencoding())
+
+# sheet_B = book_source.sheet_by_index(0)
+# print(sheet_B.name)
+
+
+
