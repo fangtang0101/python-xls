f0831c971c080e2b8f1b07be34c04c1e19ad4863
fangchungao <fangchungao@rltx.com>
2018-03-02 11:13:11 +0800

    1.一张 报表 ok

--- test.py
+++ test.py
@@ -14,79 +14,79 @@ if sys.getdefaultencoding() != defaultencoding:
 
 
 
-# # step 1  解析模板 ================================================
+# step 1  解析模板 ================================================
 
-# book = xlrd.open_workbook('模版.xls',formatting_info=True)
-# sheets=book.sheets()
+book = xlrd.open_workbook('模版.xls',formatting_info=True)
+sheets=book.sheets()
 
-# sheet_A37 = book.sheet_by_name('37#')
+sheet_A37 = book.sheet_by_name('韩乡园')
 
-# rows = sheet_A37.nrows
-# cols = sheet_A37.ncols
+rows = sheet_A37.nrows
+cols = sheet_A37.ncols
 
-# print('row',rows)
-# print('col',rows)
+print('row',rows)
+print('col',rows)
 
-# # row 列
-# # col 行
+# row 列
+# col 行
 
-# list_cell = []
+list_cell = []
 
-# for row in range(rows):
-# 	cell = sheet_A37.cell_value(row,3)
-# 	if cell.strip() != '': # 需要判断shif
-# 		cell_target  = {'row': row, 'col': 3, 'name': cell }
-# 		list_cell.append(cell_target)
-# 		# print(cell.encode('utf-8'))
+for row in range(rows):
+	cell = sheet_A37.cell_value(row,3)
+	if cell.strip() != '': # 需要判断shif
+		cell_target  = {'row': row, 'col': 3, 'name': cell }
+		list_cell.append(cell_target)
+		# print(cell.encode('utf-8'))
 
 		
 	    
-# # print(list_cell)
+# print(list_cell)
 
 
-# #  step2    anther sheet find value  ================================================
+#  step2    anther sheet find value  ================================================
 
 
 
-# book_source = xlrd.open_workbook('./1704/RY01销售量排名.xls',formatting_info=True)
+book_source = xlrd.open_workbook('./1704/RY01销售量排名.xls',formatting_info=True)
 
-# sheet_B = book_source.sheet_by_index(0)
+sheet_B = book_source.sheet_by_index(0)
 
-# # 拿到所有的  list
+# 拿到所有的  list
 
-# rows_sou = sheet_B.nrows
-# cols_sou = sheet_B.ncols
+rows_sou = sheet_B.nrows
+cols_sou = sheet_B.ncols
 
-# list_sou = []
+list_sou = []
 
-# for row in range(rows_sou):
-# 	cell = sheet_B.cell_value(row,1)
-# 	if cell.strip() != '': # 需要判断shif
-# 		cell_target  = {'row': row, 'col': 1, 'name': cell ,'val1':sheet_B.cell_value(row,3), 'val2':sheet_B.cell_value(row,4)}
+for row in range(rows_sou):
+	cell = sheet_B.cell_value(row,1)
+	if cell.strip() != '': # 需要判断shif
+		cell_target  = {'row': row, 'col': 1, 'name': cell ,'val1':sheet_B.cell_value(row,4), 'val2':sheet_B.cell_value(row,5)}
 
-# 		# val1 = sheet_B.cell_value(row,3)
-# 		# val2 = sheet_B.cell_value(row,4)
-# 		# cell_target.val1 = val1
-# 		# cell_target.val2 = val2
-# 		list_sou.append(cell_target)
+		# val1 = sheet_B.cell_value(row,3)
+		# val2 = sheet_B.cell_value(row,4)
+		# cell_target.val1 = val1
+		# cell_target.val2 = val2
+		list_sou.append(cell_target)
 
-# # print(list_sou)	
-
-
-# # step 3  合并 数组
-
-# list_all = []
-
-# for item_sou in list_sou:
-#     for item_target in list_cell:
-#  	    if item_sou['name'] == item_target['name']:
-#  		    item_target['val1'] = item_sou['val1']
-#  		    item_target['val2'] = item_sou['val2']
-#  		    list_all.append(item_target)
-#  		    break
+# print(list_sou)	
 
 
-# print(list_all)
+# step 3  合并 数组
+
+list_all = []
+
+for item_sou in list_sou:
+    for item_target in list_cell:
+ 	    if item_sou['name'] == item_target['name']:
+ 		    item_target['val1'] = item_sou['val1']
+ 		    item_target['val2'] = item_sou['val2']
+ 		    list_all.append(item_target)
+ 		    break
+
+
+print(list_all)
 
 
 
@@ -97,8 +97,12 @@ if sys.getdefaultencoding() != defaultencoding:
 
 rb = xlrd.open_workbook('模版.xls',formatting_info=True)
 wb = copy(rb)
-ws = wb.get_sheet(0)
-ws.write(0, 0, 'changed!')
+ws = wb.get_sheet(3)
+
+for item in list_all:
+	ws.write(item['row'], item['col']+2, item['val1'])
+	ws.write(item['row'], item['col']+3, item['val2'])
+
 wb.save('3.xls')
 
 
